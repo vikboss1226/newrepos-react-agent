6f114a4e9589192993688d16317a05f283c11d02
vikboss1226 <vikboss1226@gmail.com>
2026-07-10 20:32:29 +0530

    files changed

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders the app heading', () => {
   render(<App />);
-  const heading = screen.getByText(/weather no/i);
+  const heading = screen.getByText(/testtg/i);
   expect(heading).toBeInTheDocument();
 });
